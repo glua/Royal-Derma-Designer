259d1b33c9d608b95b43287abcec566fca80a796
Marcel <mariobartholome@web.de>
2015-07-07 11:59:52 +0200

    delete

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -45,46 +45,7 @@ local footer =
 	include( "designer/core/cl_main.lua" )
 	MsgC( Color( 255, 0, 0 ), footer .. "\n" )
 
-	function PointOnCircle( ang, radius, offX, offY )
-	ang = math.rad( ang )
-	local x = math.cos( ang ) * radius + offX
-	local y = math.sin( ang ) * radius + offY
-	return x, y
-	end
-
-	function ASTS()
-
-	 
-	end
-
-	hook.Add("HUDPaint", "draw", ASTS )
 
 
-
-
-	local val = 1
-
-	function nullstelle( a,b,c,d,max )
-
-
-
-	print( "" .. a[1] .. " * " .. val .. " ^" .. a[2] .. " " .. 0+ b[1] .. " * " ..  val .. "^ " .. b[2] .. " " .. 0+ c[1] .. " * " .. val .. "^ " .. c[2] .. " + " .. 0 + d[1] .. " = " ..  ( a[1] * val ^ a[2] ) + ( b[1] * val ^ b[2] ) + ( c[1] * val ^ c[2] ) + d[1] )
-	if( val == 0 ) then print( "sorry zu gross" ) val = 1 return  end
-
-	if( a[1] * val ^a[2] + b[1] * val ^ b[2] + c[1] * val ^ c[2] + d[1] != 0 ) then
-		if( val > max ) then
-			val = -max
-		end
-		val = val + 1
-		nullstelle( a,b,c,d,max )
-
-	else
-
-		print( val )
-
-	end
-	end
-
---plyInput = { "!announce", "Du", "bist", "eine","kleine", "schwuchtel" } local message = "" if( #plyInput > 2 ) then for k,v in ipairs( plyInput ) do if( k == 1 ) then else message = message .. " " .. v end end end print( message )
-
+	
 
